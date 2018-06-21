266992c93c8a95221fa208b0c1960a6e10b1719b
Sun1ive <sunliveua@gmail.com>
2018-06-21 10:38:43 +0300

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 export default new Router({
-  base: '/personal/',
+  base: process.env.NODE_ENV === 'production' ? '/personal/' : '/',
   mode: 'history',
   routes: [
     {
