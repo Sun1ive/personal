7e1a76577a53675540629efa5200d4268f2a4075
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-26 07:16:09 +0000

    Deploy Sun1ive/personal to github.com/Sun1ive/personal.git:gh-pages

---
+++ precache-manifest.9122d9fc9d0675548888f61baab88b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a7e2f8c6d6ff07722d9",
+    "url": "/personal/js/chunk-vendors.8a7e2f8c.js"
+  },
+  {
+    "revision": "f3a8ef452479afeb6735",
+    "url": "/personal/js/app.f3a8ef45.js"
+  },
+  {
+    "revision": "f94ba571de7f7a94c110",
+    "url": "/personal/js/0.f94ba571.js"
+  },
+  {
+    "revision": "c8818e306ff3ac533f019ff5bc522db1",
+    "url": "/personal/index.html"
+  },
+  {
+    "revision": "b0ae3a6d8cab4102a806ac108c7895ec",
+    "url": "/personal/img/bg.b0ae3a6d.jpg"
+  },
+  {
+    "revision": "f3a8ef452479afeb6735",
+    "url": "/personal/css/app.9a344b60.css"
+  },
+  {
+    "revision": "f94ba571de7f7a94c110",
+    "url": "/personal/css/0.62a82972.css"
+  }
+];
